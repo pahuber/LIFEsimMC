3667a220a26f27aa7787de8ca246a846c2ce53f8
pahuber <philhuber@live.com>
2024-02-19 15:40:36 +0100

    Add basic classes

---
+++ lifesim2/core/context.py
@@ -0,0 +1,9 @@
+class Context:
+    """Class representation of the context."""
+
+    def __init__(self):
+        self.settings = None
+        self.observation = None
+        self.observatory = None
+        self.scene = None
+        self.data = None
---
+++ lifesim2/core/pipeline.py
@@ -0,0 +1,24 @@
+from lifesim2.core.base_module import BaseModule
+from lifesim2.core.context import Context
+
+
+class Pipeline:
+    """Class representation of the pipeline."""
+
+    def __init__(self):
+        """Constructor method."""
+        self._modules = []
+        self._context = Context()
+
+    def add_module(self, module: BaseModule):
+        """Add a module to the pipeline.
+
+        :param module: The module to add
+        """
+        self._modules.append(module)
+
+    def run(self):
+        """Run the pipeline with all the modules that have been added. Remove the modules after running."""
+        for index_module, module in enumerate(self._modules):
+            self._context = module.apply(context=self._context)
+            self._modules.pop(index_module)
