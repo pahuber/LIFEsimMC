Fix testing and variance

lifesimmc/core/modules/processing/mcmc_module.py
@@ -159,7 +159,8 @@ class MCMCModule(BaseModule):
             # Define data uncertainty
             # yerr = np.ones(len(data)) * 1.5 * np.max(abs(get_model(time, *initial_guess)))
             yerr = np.ones(len(data_in)) * 0.1 * np.max(abs(data_in))
-            yerr = np.var(data_in, axis=1)
+            yerr = np.sqrt(np.var(data_in, axis=1))
+            # print(yerr)
             # yerr = 0.8 * (data - get_model(time, *initial_guess))
             # print(yerr.shape)
             # yerr = 0.1 * data

lifesimmc/core/modules/testing/neyman_pearson_test_module.py
@@ -95,10 +95,10 @@ class NeymanPearsonTestModule(BaseModule):
                 y_pos,
                 flux_in
             )[i, :, :, 0, 0]).flatten()
-            xtx = (model.T.dot(model)) / ndim
+            xtx = (model.T.dot(model))  # / ndim
             # pfa = 0.0001
             xsi = np.sqrt(xtx) * norm.ppf(1 - self.pfa)
-            test = (dataf @ model) / ndim
+            test = (dataf @ model)  # / ndim
 
             r_test_out = TestResource(
                 name='',