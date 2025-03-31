Fix numerical signal extraction with orbital motion

lifesimmc/core/modules/processing/analytical_mle_module.py
@@ -145,10 +145,18 @@ class AnalyticalMLEModule(BaseModule):
 
                 index_x, index_y = torch.nonzero(sky_brightness_distribution[0], as_tuple=True)
                 # index_x, index_y = index_x[0].item(), index_y[0].item()
-                x_coord = config.phringe._director._planets[0].sky_coordinates[
-                    0, index_x[0].item(), index_y[0].item()].cpu().numpy()
-                y_coord = config.phringe._director._planets[0].sky_coordinates[
-                    1, index_x[0].item(), index_y[0].item()].cpu().numpy()
+
+                # if has orbital motion, use position a at initial time
+                if config.phringe._director._planets[0].sky_coordinates.ndim == 4:
+                    x_coord = config.phringe._director._planets[0].sky_coordinates[
+                        0, 0, index_x[0].item(), index_y[0].item()].cpu().numpy()
+                    y_coord = config.phringe._director._planets[0].sky_coordinates[
+                        1, 0, index_x[0].item(), index_y[0].item()].cpu().numpy()
+                else:
+                    x_coord = config.phringe._director._planets[0].sky_coordinates[
+                        0, index_x[0].item(), index_y[0].item()].cpu().numpy()
+                    y_coord = config.phringe._director._planets[0].sky_coordinates[
+                        1, index_x[0].item(), index_y[0].item()].cpu().numpy()
                 # print(index_x, index_y)
                 # plt.imshow(sky_brightness_distribution[0].cpu().numpy())
                 # plt.colorbar()

lifesimmc/core/modules/processing/numerical_mle_module.py
@@ -70,10 +70,10 @@ class NumericalMLEModule(BaseModule):
         posx = np.array(r_coordinates_in.x) if r_coordinates_in is not None else np.array(hfov_max / 2)
         posy = np.array(r_coordinates_in.y) if r_coordinates_in is not None else np.array(hfov_max / 2)
 
-        # Handle orbital motion
-        if posx.size > 1:
-            posx = posx[0]
-            posy = posy[0]
+        # # Handle orbital motion
+        # if posx.size > 1:
+        #     posx = posx[0]
+        #     posy = posy[0]
 
         for i in range(len(flux_init)):
             # params.add(f'flux_{i}', value=0.9 * flux_init[i], min=0, max=1e7)