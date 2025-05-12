c08efa6c2a3a23b73c1258c8121e5af718497b32
pahuber <philhuber@live.com>
2025-05-12 17:10:43 +0200

    Fix orbital motion

--- lifesimmc/core/modules/loading/setup_module.py
+++ lifesimmc/core/modules/loading/setup_module.py
@@ -162,6 +162,11 @@ class SetupModule(BaseModule):
             sky_brightness_distribution = planet._sky_brightness_distribution
             non_zero_indices = torch.nonzero(sky_brightness_distribution[0])
             sky_coordinates = planet._sky_coordinates
+
+            # If planet has orbital motion, i.e. sky_coordinates change with time, then use the first time step
+            if planet.has_orbital_motion:
+                sky_coordinates = sky_coordinates[:, 0]
+
             pos_x = sky_coordinates[0][non_zero_indices[0][0], non_zero_indices[0][1]].item()
             pos_y = sky_coordinates[1][non_zero_indices[0][0], non_zero_indices[0][1]].item()
 
